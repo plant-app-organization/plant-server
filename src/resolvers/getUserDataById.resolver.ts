9d4f3b657cc7ce82090c0fc9bfff433a85eac6da
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-03-24 14:51:16 +0100

    fix getuserdatabyid resolver

--- src/resolvers/getUserDataById.resolver.ts
+++ src/resolvers/getUserDataById.resolver.ts
@@ -8,21 +8,10 @@ import clerk, { sessions } from '@clerk/clerk-sdk-node'
 export class GetUserDataByIdResolver {
   constructor(private prisma: PrismaService) {}
   @Mutation(() => String)
-  async getUserDataById(@Context() context, @Args('userId') userId: string): Promise<User> {
-    console.log('🔥userId dans le resolver GetUserDataByIdResolver', userId)
-    const authorizationHeader = context.req.headers.authorization
-    const token = authorizationHeader.split(' ')[1] // extract the token from the header
-    console.log('token dans le header', token)
-
-    const client = await clerk.clients.verifyClient(token)
-    console.log('client', client)
-    console.log('userId', client.sessions[0].userId)
-    const user = await clerk.users.getUser(client.sessions[0].userId)
-    console.log('🪴user', user)
-
+  async getUserDataById(@Args('userId') userId: string): Promise<User> {
     const foundUser = await this.prisma.user.findUnique({
       where: {
-        clerkId: client.sessions[0].userId,
+        id: userId,
       },
     })
 
