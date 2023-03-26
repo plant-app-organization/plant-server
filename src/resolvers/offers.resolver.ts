cdd811f875d8e29caef2d7b0c30406acac896af5
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-03-27 00:03:02 +0200

    better search engine

--- src/resolvers/offers.resolver.ts
+++ src/resolvers/offers.resolver.ts
@@ -16,23 +16,6 @@ export class GetOffersResolver {
     @Args('filters', { type: () => [String] }) filters: string[],
   ): Promise<Offer[]> {
     console.log('🔥filters dans resolver getOffers', filters)
-    // const authorizationHeader = context.req.headers.authorization
-    // const token = authorizationHeader.split(' ')[1] // extract the token from the header
-    // // console.log('token dans le header', token)
-
-    // const client = await clerk.clients.verifyClient(token)
-    // console.log('client', client)
-    // console.log('userId', client.sessions[0].userId)
-    // const user = await clerk.users.getUser(client.sessions[0].userId)
-    // console.log('🪴user', user)
-
-    // const foundUser = await this.prisma.user.findUnique({
-    //   where: {
-    //     clerkId: client.sessions[0].userId,
-    //   },
-    // })
-
-    // console.log('foundUser', foundUser)
 
     try {
       const where = filters?.length
@@ -50,6 +33,57 @@ export class GetOffersResolver {
     } catch (error) {
       // If an error occurred, return false
       throw new Error('Failed to get offers')
+    }
+  }
+
+  @Query((returns) => [Offer], {
+    name: 'OffersListSearch',
+    description: 'Get List of Offers Searched',
+  })
+  async searchOffers(
+    @Context() context,
+    @Args('searchInput', { type: () => String }) searchInput: string,
+    @Args('filters', { type: () => [String] }) filters: string[],
+  ): Promise<Offer[]> {
+    console.log('🔎searchInput dans resolver getOffers', searchInput)
+    console.log('🔥filters dans resolver getOffers', filters)
+
+    try {
+      const foundOffers = await this.prisma.offer.findMany({
+        where: {
+          AND: [
+            {
+              OR: [
+                {
+                  plantName: {
+                    contains: searchInput,
+                    mode: 'insensitive',
+                  },
+                },
+                {
+                  description: {
+                    contains: searchInput,
+                    mode: 'insensitive',
+                  },
+                },
+              ],
+            },
+            {
+              OR: filters?.length
+                ? filters.map((filter) => ({ category: { contains: filter } }))
+                : {},
+            },
+          ],
+        },
+        orderBy: {
+          updatedAt: 'desc',
+        },
+      })
+
+      return foundOffers
+    } catch (error) {
+      // If an error occurred, return false
+      throw new Error('Failed to find offers')
     }
   }
 }
