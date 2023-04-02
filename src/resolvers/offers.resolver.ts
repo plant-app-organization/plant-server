b5cc9e8cc88a4181ce0542c4e7c96c618379a5a0
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-04-02 22:38:46 +0200

    fix resolver getoffers when user is not signed in

--- src/resolvers/offers.resolver.ts
+++ src/resolvers/offers.resolver.ts
@@ -51,63 +51,105 @@ export class GetOffersResolver {
     const authorizationHeader = context.req.headers.authorization
     const token = authorizationHeader.split(' ')[1] // extract the token from the header
     console.log('token dans le header', token)
+    if (token) {
+      const client = await clerk.clients.verifyClient(token)
+      // console.log('client', client)
+      // console.log('userId', client.sessions[0].userId)
+      const user = await clerk.users.getUser(client.sessions[0].userId)
+      // console.log('🪴user', user)
 
-    const client = await clerk.clients.verifyClient(token)
-    // console.log('client', client)
-    // console.log('userId', client.sessions[0].userId)
-    const user = await clerk.users.getUser(client.sessions[0].userId)
-    // console.log('🪴user', user)
-
-    const foundUser = await this.prisma.user.findUnique({
-      where: {
-        clerkId: client.sessions[0].userId,
-      },
-    })
-
-    console.log('foundUser', foundUser)
-    try {
-      const foundOffers = await this.prisma.offer.findMany({
+      const foundUser = await this.prisma.user.findUnique({
         where: {
-          AND: [
-            {
-              OR: [
-                {
-                  plantName: {
-                    contains: searchInput,
-                    mode: 'insensitive',
-                  },
-                },
-                {
-                  description: {
-                    contains: searchInput,
-                    mode: 'insensitive',
-                  },
-                },
-              ],
-            },
-            {
-              OR: filters?.length
-                ? filters.map((filter) => ({ category: { contains: filter } }))
-                : {},
-            },
-          ],
-        },
-        orderBy: {
-          createdAt: 'desc',
+          clerkId: client.sessions[0].userId,
         },
       })
-      // console.log('🔥foundOffers', foundOffers)
-      const bookmarkedOffers = foundUser.bookmarks
 
-      const foundOffersWithBookmarks = foundOffers.map((el) => {
-        console.log('liké ou pas', bookmarkedOffers.includes(el.id))
-        return { ...el, isBookmarked: bookmarkedOffers.includes(el.id) }
-      })
-      // console.log('foundOffersWithBookmarks', foundOffersWithBookmarks)
-      return foundOffersWithBookmarks
-    } catch (error) {
-      // If an error occurred, return false
-      throw new Error('Failed to find offers')
+      console.log('foundUser', foundUser)
+
+      try {
+        const foundOffers = await this.prisma.offer.findMany({
+          where: {
+            AND: [
+              {
+                OR: [
+                  {
+                    plantName: {
+                      contains: searchInput,
+                      mode: 'insensitive',
+                    },
+                  },
+                  {
+                    description: {
+                      contains: searchInput,
+                      mode: 'insensitive',
+                    },
+                  },
+                ],
+              },
+              {
+                OR: filters?.length
+                  ? filters.map((filter) => ({ category: { contains: filter } }))
+                  : {},
+              },
+            ],
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+        // console.log('🔥foundOffers', foundOffers)
+
+        const bookmarkedOffers = foundUser.bookmarks
+
+        const foundOffersWithBookmarks = foundOffers.map((el) => {
+          console.log('liké ou pas', bookmarkedOffers.includes(el.id))
+          return { ...el, isBookmarked: bookmarkedOffers.includes(el.id) }
+        })
+        // console.log('foundOffersWithBookmarks', foundOffersWithBookmarks)
+        return foundOffersWithBookmarks
+      } catch (error) {
+        // If an error occurred, return false
+        throw new Error('Failed to find offers')
+      }
+    } else {
+      try {
+        const foundOffers = await this.prisma.offer.findMany({
+          where: {
+            AND: [
+              {
+                OR: [
+                  {
+                    plantName: {
+                      contains: searchInput,
+                      mode: 'insensitive',
+                    },
+                  },
+                  {
+                    description: {
+                      contains: searchInput,
+                      mode: 'insensitive',
+                    },
+                  },
+                ],
+              },
+              {
+                OR: filters?.length
+                  ? filters.map((filter) => ({ category: { contains: filter } }))
+                  : {},
+              },
+            ],
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+        // console.log('🔥foundOffers', foundOffers)
+
+        return foundOffers
+      } catch (error) {
+        // If an error occurred, return false
+        throw new Error('Failed to find offers')
+      }
     }
   }
 }
