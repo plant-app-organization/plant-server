ee0c8b6017df621dc8d5131f8510ab2bd1e31053
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2023-11-21 11:58:47 +0100

    fix

--- src/resolvers/messages.resolver.ts
+++ src/resolvers/messages.resolver.ts
@@ -94,16 +94,16 @@ export class MessagesResolver {
       })
 
       // Notification push
-      // const pushNotificationResponse = await axios.post(
-      //   `https://app.nativenotify.com/api/indie/notification`,
-      //   {
-      //     subID: secondParticipant.email,
-      //     appId: 15168,
-      //     appToken: '2NQv5UM3ppjj8VIDgMfgb4',
-      //     title: `✉️ Nouveau message de ${foundUser.userName}`,
-      //     message: messageInput.text,
-      //   },
-      // )
+      const pushNotificationResponse = axios.post(
+        `https://app.nativenotify.com/api/indie/notification`,
+        {
+          subID: secondParticipant.email,
+          appId: 15168,
+          appToken: '2NQv5UM3ppjj8VIDgMfgb4',
+          title: `✉️ Nouveau message de ${foundUser.userName}`,
+          message: messageInput.text,
+        },
+      )
       // console.log('response Push Notification', pushNotificationResponse)
       // Notification email
       const msg = {
